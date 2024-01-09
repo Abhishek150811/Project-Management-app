6b0d5982ddfbc7c945b62155626eb13f7d78b7e5
Abhishek150811 <abhishekaps2003@gmail.com>
2024-01-09 17:55:38 +0530

    made side bar and central screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import NewProject from "./Components/NewProject.jsx";
+import NoProjectSelected from "./Components/NoProjectSelected.jsx";
+import ProjectSidebar  from "./Components/ProjectSidebar.jsx";
+import { useState  } from "react";
+
+
 function App() {
+  const [projectState , setProjectState] = useState({
+    selectedProjectId : undefined , 
+    projects: []
+  }) ;
+
+  function handleStateAddProject(){
+    setProjectState(prevState =>{
+      return {
+        ...prevState , 
+        selectedProjectId: null , 
+      }
+    })
+  }
+
+  let content ; 
+  if(projectState.selectedProjectId === null){
+    content = <NewProject></NewProject>
+  }
+  else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStateAddProject} ></NoProjectSelected>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8" >
+      <ProjectSidebar onStartAddProject={handleStateAddProject}></ProjectSidebar>
+      {content}
+    </main>
   );
 }
 
